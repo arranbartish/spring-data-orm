7cc3aa4d1d2443612831d7966c41d3018b5aedb8
Arran Bartish <arranbartish@hotmail.com>
2021-01-05 16:30:45 -0500

    Allow any non annotated type

--- src/main/java/online/bartish/example/orm/dao/CanNotJpaAnnotateRepository.java
+++ src/main/java/online/bartish/example/orm/dao/CanNotJpaAnnotateRepository.java
@@ -2,7 +2,7 @@ package online.bartish.example.orm.dao;
 
 import online.bartish.example.orm.model.CanNotJpaAnnotate;
 
-public interface CanNotJpaAnnotateRepository<CanNotJpaAnnotateT extends CanNotJpaAnnotate> {
+public interface CanNotJpaAnnotateRepository<CanNotJpaAnnotateT> {
 
   CanNotJpaAnnotateT findByValue1(String Value1);
 }
